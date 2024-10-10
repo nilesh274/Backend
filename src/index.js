3af972ab1d51bc772f5f6ac4e14303ccd1de2f71
nilesh274 <nileshparmarofficial274@gmail.com>
2024-10-10 13:01:08 +0530

    connection of database

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
 // nodemon is use reload the server if there is change in index.js file
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env"
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+( async () => {
+    try {
+        await mongoose.connect(`${process_params.env.MONGODB_URI}/${DB_NAME}`)
+        
+        app.on("error:", (error) => {
+            console.log("ERRR:", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listen on ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+       console.error("error", error);
+       throw error; 
+    }
+})()
+*/
